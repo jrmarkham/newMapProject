e9d624eb05a22c4310c133a611f45a5f55c719e1
John Markham <me@markhamenterprises.com>
2013-05-03 12:33:15 -0400

    jq2

--- jquery-mobile/map.js
+++ jquery-mobile/map.js
@@ -6,3 +6,12 @@ $(document).ready(function(e) {
 	$('#test').append(' From Jquery');
 
 });
+
+
+// JavaScript // JavaScript Document
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// PhoneGap is ready
+function onDeviceReady() {
+    $('#test').append(' PG - Devide ready');
+}
